08ffcf7db7881948289c92e618e2f5aa74d56974
MekiaKianian <me.kia.cmd@gmail.com>
2021-06-10 11:02:45 +0430

    initial commit

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,46 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsersController : ControllerBase
+    {
+        private readonly DataContext _context;
+        public UsersController(DataContext context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+        public ActionResult<IEnumerable<AppUser>> GetUsers()
+        {
+            return _context.Users.ToList();
+        }
+        [HttpGet("{id}")]
+        public ActionResult<AppUser> GetUser(int id)
+        {
+            return _context.Users.Find(id);
+
+        }
+
+       [HttpPost]
+        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers2()
+        {
+            return await _context.Users.ToListAsync();
+        }
+       [HttpPost("{id123}")]
+        public async Task<ActionResult<AppUser>> GetUser2(int id123)
+        {
+            return await _context.Users.FindAsync(id123);
+
+        }
+
+
+    }
+}
